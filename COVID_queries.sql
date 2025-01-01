1e6fc1ce9bbf7743dc7bee7d38d20ce8b26de50f
HomatoFeng <breakwave922@gmail.com>
2024-12-31 22:55:35 -0600

    update sql

--- COVID_queries.sql
+++ COVID_queries.sql
@@ -1,12 +1,12 @@
 -- COVID Cases Explore
 
--- death likehood by detected cases
+-- Death likehood by detected cases
 SELECT country,STR_TO_DATE(date, '%m/%d/%y') AS formatted_date,total_cases,total_deaths,total_deaths/total_cases*100 as death_rate
 FROM COVID_data.compact_covert
 WHERE continent not like '' and country = 'United States'
 ORDER BY formatted_date
 
--- global death rate
+-- Global death rate
 SELECT sum(new_cases) as total_cases,sum(new_deaths) as total_death,sum(new_deaths)/sum(new_cases)*100 as death_rate
 FROM COVID_data.compact_covert
 WHERE continent not like '' 
@@ -17,22 +17,21 @@ FROM COVID_data.compact_covert
 WHERE continent not like '' -- and country = 'United States'
 order by country,formatted_date
 
--- country with highes infection rate by population
+-- Country with highes infection rate by population
 SELECT country,population,max(total_cases) as max_cases,max(total_cases/population)*100 as Max_detect_rate
 FROM COVID_data.compact_covert
 WHERE continent not like ''
 GROUP BY country,population
 ORDER BY Max_detect_rate DESC
 
-
--- country with highes death rate compared to population
+-- Country with highes death rate compared to population
 SELECT country,max(total_deaths) as max_deaths
 FROM COVID_data.compact_covert
 WHERE continent not like ''
 GROUP BY country
 ORDER BY max_deaths desc
 
--- total death count by continent
+-- Total death count by continent
 SELECT continent,sum(max_deaths) as total_deaths
 FROM
 (SELECT continent,country,max(total_deaths) as max_deaths
@@ -42,7 +41,7 @@ GROUP BY continent,country) as tmp
 GROUP BY continent
 order by total_deaths desc
 
--- continents with the highest death count per population
+-- Continents with the highest death count per population
 SELECT continent,sum(max_deaths)/sum(population)*100 as deaths_rate_continent
 FROM
 (SELECT continent,country,population,max(total_deaths) as max_deaths
@@ -52,14 +51,14 @@ GROUP BY continent,country,population) as tmp
 GROUP BY continent
 ORDER BY deaths_rate_continent DESC
 
--- from global perspective
+-- From global perspective
 SELECT STR_TO_DATE(date, '%m/%d/%y') AS formatted_date,sum(new_cases) as total_new_cases,sum(new_deaths) as total_new_deaths, sum(new_deaths)/sum(new_cases)*100 as deathpercentage
 FROM COVID_data.compact_covert
 WHERE continent not like ''
 GROUP BY formatted_date
 ORDER BY formatted_date
 
--- look at total vaccination numbern and percentage
+-- Look at total vaccination numbern and percentage
 WITH acc_vac_tb as (SELECT continent,country,STR_TO_DATE(date, '%m/%d/%y') AS formatted_date,new_vaccinations,sum(new_vaccinations) over(partition by country order by country,STR_TO_DATE(date, '%m/%d/%y')) as acc_vac, population 
 FROM COVID_data.compact_covert
 WHERE continent not like ''
